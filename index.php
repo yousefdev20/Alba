de87e304f22d534acd1d6f84a0b662f3c6d5c3e0
Yousef <yousef.dev20@gmail.com>
2024-03-27 03:58:03 +0200

    First Release

--- index.php
+++ index.php
@@ -5,5 +5,3 @@ use Yousef\Alba\System\Core\App\Application;
 require __DIR__ . '/vendor/autoload.php';
 
 (new Application())->handelIncomingRequest();
-
-die((\Yousef\Alba\System\Core\App\Container::getInstance()->routes[1]));
